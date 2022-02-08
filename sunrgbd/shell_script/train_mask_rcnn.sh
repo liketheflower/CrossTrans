265df5cbe161666f7bf3cdc66353f9c5d420e433
jimmy shen <jim.morris.shen@gmail.com>
2022-02-08 17:44:25 -0500

    Add maskrcnn without intertrans res

--- sunrgbd/shell_script/train_mask_rcnn.sh
+++ sunrgbd/shell_script/train_mask_rcnn.sh
@@ -3,5 +3,5 @@
 #python tools/train.py  ${cfg} > train_maskrcnn_with_intertrans_new_coco_dataset.log
 
 # Train without interTrans  train from scratch
-cfg= configs/mask_rcnn/mask_rcnn_r50_fpn_mstrain-poly_3x_coco_no_pretrain.py 
+cfg=configs/mask_rcnn/mask_rcnn_r50_fpn_mstrain-poly_3x_coco_no_pretrain.py 
 python tools/train.py  ${cfg} > train_maskrcnn_with_intertrans_new_coco_dataset_no_pretrain.log
